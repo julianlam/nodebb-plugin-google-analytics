aeb4e3daca0f043f0451b5e16c413184f5803148
gasoved <gasoved@gmail.com>
2021-05-07 18:23:35 +0300

    fix: dont use a local copy of settings

--- library.js
+++ library.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const winston = require.main.require('winston');
 const meta = require.main.require('./src/meta');
 
 const controllers = require('./lib/controllers');
@@ -15,30 +14,11 @@ plugin.init = async function(params) {
 	const routeHelpers = require.main.require('./src/routes/helpers');
 
 	routeHelpers.setupAdminPageRoute(router, `/admin/plugins/${PLUGIN_NAME}`, middleware, [], controllers.renderAdminPage);
-
-	// Load asset ID from config
-	await plugin.loadSettings();
 };
 
 plugin.filterConfigGet = async function (config) {
-	config[PLUGIN_NAME] = plugin.settings;
+	config[PLUGIN_NAME] = await meta.settings.get(PLUGIN_NAME);
 	return config;
-};
-
-plugin.loadSettings = async () => {
-	const settings = await meta.settings.get(PLUGIN_NAME);
-	if (settings.id || settings.ga4id) {
-		settings.useUA = settings.useUA === 'on' || false;
-		plugin.settings = settings;
-	} else {
-		winston.error('A Google Analytics ID (e.g. G-XXXXXXXXXX) was not specified. Please complete setup in the administration panel.');
-	}
-};
-
-plugin.onConfigChange = async (hash) => {
-	if (hash === `settings:${PLUGIN_NAME}`) {
-		await plugin.loadSettings();
-	}
 };
 
 plugin.addAdminNavigation = async (header) => {
